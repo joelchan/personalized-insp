8a6e28f8bef026ba3fdf48db8ab52224bc3c167e
Peter Kremer <pkremer3@u.rochester.edu>
2014-06-29 22:13:25 -0400

    included d3 via meteor

--- brainstorm/client/dashboard.js
+++ brainstorm/client/dashboard.js
@@ -1,3 +1,36 @@
+Template.Dashboard.rendered = function(){
+
+	var w = 400;
+	var h = 50;
+	var ideasFreq = [2, 5, 3, 1, 2, 0, 2, 1, 2, 0, 1];
+
+	var hist =	d3.select(".series")
+					.append("svg")
+					.attr("width", w)
+					.attr("height", h);
+
+	hist.selectAll("rect")
+		.data(ideasFreq)
+		.enter()
+		.append("rect")
+		.attr("x", function(d, i){
+			return i * (w / ideasFreq.length) 
+		})
+		.attr("y", function(d){
+			return h - (d * 5);
+		})
+		.attr("width", function(d, i){
+			return (w / ideasFreq.length)-1 
+		})
+		.attr("height", function(d){
+			return (d * 5);
+		})
+		.attr("fill", function(d){
+			return "rgb("+ (50-10*d) +","+ (118-10*d) +","+ (177-10*d)+")";
+		});
+
+}
+
 /********************************************************************
 * Template Helpers
 *********************************************************************/
@@ -8,7 +41,11 @@ Template.Dashboard.helpers({
 
   	gamechangers : function(){
   		return false;
-  	}
+  	},
+
+  	clusters : function(){
+    	return Clusters.find().fetch();
+  	},
 });
 
 /********************************************************************
