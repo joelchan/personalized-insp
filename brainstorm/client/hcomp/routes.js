133979d95c9ab142243951f933ac84a6a4b58d9c
Steven Dang <stevencdang@gmail.com>
2014-10-16 15:28:24 +0000

    reorganized client folder files

---
+++ brainstorm/client/hcomp/routes.js
@@ -0,0 +1,38 @@
+// Configure logger for Tools
+var logger = new Logger('Client:Hcomp:Routes');
+// Comment out to use global logging level
+Logger.setLevel('Client:Hcomp:Routes', 'trace');
+//Logger.setLevel('Client:Hcomp:Routes', 'debug');
+//Logger.setLevel('Client:Hcomp:Routes', 'info');
+//Logger.setLevel('Client:Hcomp:Routes', 'warn');
+
+//Maps routes to templates
+Router.map(function () {
+  /***************************************************************
+   * Define custom routes for hcomp pages
+   * *************************************************************/
+  //this.route('MTurkLoginPage', {
+  	//path: '/MturkLoginPage/:_id',
+    //template: 'MTurkLoginPage',
+    //waitOn: function() {
+      //return Meteor.subscribe('prompts', this.params._id);
+    //},
+    //onBeforeAction: function() {
+        //console.log("before action");
+        //if (this.ready()) {
+          //console.log("Data ready");
+          //var prompt = Prompts.findOne({_id: this.params._id});
+          //if (prompt) {
+            //Session.set("currentPrompt", prompt);
+          //}
+        //}
+    //},
+    //action: function(){
+      //if(this.ready())
+        //this.render();
+      //else
+        //this.render('loading');
+    //}, 
+  //});
+});
+
