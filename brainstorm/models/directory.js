64602ddb9efb8b33184b81c0bf740b0a05d280f5
Steven Dang <stevencdang@gmail.com>
2014-10-21 01:43:49 -0400

    Converted to bootstrap 3.2, and shifted files to repair some issues introduced by file restructuring

---
+++ brainstorm/models/directory.js
@@ -0,0 +1,31 @@
+// Manually define dictionary of all collections
+MyCollections = 
+{
+    "ideas": Ideas,
+    "prompts": Prompts,
+    "replayIdeas": ReplayIdeas,
+    "myUsers": MyUsers,
+    "roles": Roles,
+    "groups": Groups,
+    'groupTemplates': GroupTemplates,
+    'clusters': Clusters,
+    'ideasToProcess': IdeasToProcess,
+    'filters': Filters,
+    'notifications': Notifications,
+    'experiments': Experiments,
+    'exp-conditions': Conditions,
+    'consents': Consents,
+    'participants': Participants,
+    'surveyResponses': SurveyResponses
+};
+//myCollections = (function() {
+  //console.log("running init of all collections dictionary");
+  //var allCols = {};
+  //for(var key in window) {
+    //var value = window[key];
+    //if (value instanceof Meteor.Collection) {
+      //allCols[value._name] = value;
+    //}
+  //}
+  //return allCols; 
+//}());
