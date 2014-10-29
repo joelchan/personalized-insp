55c9443977b5ecf9f3e7c50d1d9b6862a75369d4
Steven Dang <stevencdang@gmail.com>
2014-10-29 17:44:57 +0000

    - tweaked prompts page to add top margin - added separate run scripts for localhost and rackspace

--- brainstorm/run.sh
+++ brainstorm/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-NO_MIRROR=1 METEOR_MOCHA_TEST_DIRS=tests  ROOT_URL="http://protolab.cs.cmu.edu" meteor run --release 0.9.4
+NO_MIRROR=1 METEOR_MOCHA_TEST_DIRS=tests meteor run --release 0.9.4
