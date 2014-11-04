d99df374457c2287ad121fe5146b5240de560afc
Steven Dang <stevencdang@gmail.com>
2014-11-04 00:24:35 -0500

    testing legiontools integration on heroku

--- client/hcomp/legiontools.js
+++ client/hcomp/legiontools.js
@@ -37,6 +37,7 @@ Template.LegionFinalPage.rendered = function() {
     var regex = new RegExp(regexS);
     var results = regex.exec(window.location.href);
     console.log(window.location.href);
+    console.log(document.referrer);
     console.log(results);
     if(results == null) {
       console.log("not results in window location href");
