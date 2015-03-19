d7e285d9fae772288d1de228b7c65a6627aaed29
Steven Dang <stevencdang@gmail.com>
2015-03-19 08:14:14 -0400

    - small updates on dataforest import script

--- private/scripts/text_to_simgraph.py
+++ private/scripts/text_to_simgraph.py
@@ -39,7 +39,6 @@ if __name__  == '__main__':
     # get bag of words
     data, expandedText = nlp.bag_of_words(ideas, stopWords);
 
-    print data[0]
     # prepare dictionary
     dictionary = corpora.Dictionary(expandedText)
 
@@ -107,10 +106,10 @@ if __name__  == '__main__':
 
     #nodes = [d['id'] for d in data]
     G.add_edges_from(edges)
-    # print "nodes: %d" % G.number_of_nodes()
-    # print "edges: %d" % G.number_of_edges()
+    print "nodes: %d" % G.number_of_nodes()
+    print "edges: %d" % G.number_of_edges()
 
     solve = solver(G)
     clusters = solver.run(solve)
     clusters.sort(key=len, reverse=True)
-    # print clusters
+    print clusters
