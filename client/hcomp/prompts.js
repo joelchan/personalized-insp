7c33c515a28759c186c0f63a6853b431f2946244
Steven Dang <stevencdang@gmail.com>
2015-04-06 10:51:16 -0400

    cleaned up unused events in prompts page

--- client/hcomp/prompts.js
+++ client/hcomp/prompts.js
@@ -50,12 +50,6 @@ Template.DataProcessingTab.helpers({
 /********************************************************************
  * Prompts tab helpers and events
  * *****************************************************************/
-/********************************************************************
- * Experiments tab helpers and events
- * *****************************************************************/
-/********************************************************************
- * Data Processing tab helpers and events
- * *****************************************************************/
 Template.CrowdPromptPage.rendered = function() {
   window.scrollTo(0,0);
 }
@@ -69,27 +63,175 @@ Template.CrowdBrainstorm.rendered = function() {
     $(elm).css('margin-top', mh);
   });
 };
-
-Template.NewExperimentModal.helpers({
-  prompts: function() {
-    logger.trace("Getting prompts...");
-    return Prompts.find();
-  },
-});
-
-Template.ExperimentPromptItem.helpers({
+Template.CrowdBrainstorm.helpers({
   question: function() {
     return this.question;
   },
-  timeLimit: function() {
-    if (this.length > 0) {
-      return this.length;
+  getData: function() {
+    logger.debug("Data context: " + JSON.stringify(this._id));
+    logger.debug("current user: " + JSON.stringify(Session.get("currentUser")));
+    var result = {'promptID': this._id, 'userID': Session.get("currentUser")._id};
+    logger.debug("Data object: " + JSON.stringify(result));
+    return result;
+  },
+  participants: function() {
+    logger.debug("current prompt: " + JSON.stringify(this));
+    var groups = Groups.find({_id: {$in: this.groupIDs}});
+    var users = [];
+    groups.forEach(function(group) {
+      users = users.concat(group.users);
+    });
+    return users.map(function(user) {
+      return user.name;
+    });
+  },
+  hasTime: function() {
+    if (this.hasOwnProperty("length")) {
+      logger.trace("Prompt has session length defined: " + this.length);
+      if (this.length > 0) {
+        return true;
+      } else {
+        return false;
+      } 
     } else {
-      return "No time limit";
+      logger.trace("Prompt does not have session length defined");
+      return false;
+    }
+  },
+  hasUsers: function() {
+    var groups = Groups.find({_id: {$in: this.groupIDs}});
+    var users = [];
+    groups.forEach(function(group) {
+    logger.debug("Looking at groups: " + JSON.stringify(group));
+      logger.debug("group has users: " + JSON.stringify(group.users));
+      users = users.concat(group.users);
+      logger.debug("users list is now: " + JSON.stringify(users));
+    });
+    logger.debug("list of users:" + JSON.stringify(users));
+    if (users.length > 0) {
+      logger.trace("Prompt has users");
+      return true;
+    } else {
+      logger.trace("Prompt has no users");
+      return false;
+    }
+  },
+  numWorkers: function() {
+    logger.debug("current prompt: " + JSON.stringify(this));
+    var groups = Groups.find({_id: {$in: this.groupIDs}});
+    var users = [];
+    groups.forEach(function(group) {
+      users = users.concat(group.users);
+    });
+    return users.length;
+  },
+  formUrl: function() {
+    //Get absolute base url and trim trailing slash
+    return Meteor.absoluteUrl().slice(0,-1);
+  },
+  promptID: function() {
+    return {promptID: this._id};
+  },
+  isNotPrepped: function() {
+    var prompt = Prompts.findOne({_id: this._id})
+    logger.trace(prompt)
+    if (typeof this['forestGraphID'] === 'undefined') {
+      return true;
+    } else {
+      return false;      
     }
   },
 });
 
+Template.CrowdBrainstorm.events({
+  //'click .dash-button': function() {
+    //console.log("go to dash");
+    //Router.go("HcompDashboard", 
+      //{promptID: this._id,
+        //userID: Session.get('currentUser')});
+  //},
+  //'click .review-button': function() {
+    //console.log("go to reviewpage");
+    //Router.go("HcompResultsPage", 
+      //{promptID: this._id, userID: Session.get("currentUser")});
+  //},
+  'click .prep-forest': function () {
+    logger.debug("Prepping db for dataforest analysis");
+    logger.debug(this);
+    var prompt = this;
+    var group = Groups.findOne({_id: prompt.groupIDs[0]});
+    var user = Session.get("currentUser");
+    var type = "data_forest";
+    var data = {is_processed: false};
+    graphID = GraphManager.createGraph(prompt, group, user, type, data);
+    Prompts.update({_id: prompt._id}, {$set: {forestGraphID: graphID}});
+  },
+});
+
+
+Template.PromptsTab.events({
+    'click button.createPrompt': function () {
+      var newQuestion = $("input#prompt-text").val();
+      var newTitle = $("input#prompt-title").val();
+      var minutes = parseInt($("input#prompt-length").val());
+      logger.debug("Session will be: " + minutes + " long");
+      var newPrompt = PromptManager.create(newQuestion);
+      if (newTitle.trim() !== "") {
+        PromptManager.setTitle(newPrompt, newTitle);
+      }
+      if (!isNaN(minutes)) {
+        PromptManager.setLength(newPrompt, parseInt(minutes));
+      }
+      var group = GroupManager.create(newPrompt.template);
+      PromptManager.addGroups(newPrompt, group);
+      GroupManager.addUser(group, Session.get("currentUser"),
+          RoleManager.defaults['HcompFacilitator'].title);
+      //Clear textfield values
+      $("input#prompt-text").val("");
+      $("input#prompt-title").val("");
+      $("input#prompt-length").val(0);
+      
+    },
+    //'click .dash-button': function () {
+      //// Set the current prompt
+      //var prompt = Prompts.findOne({'_id': this._id});
+      //Session.set("currentPrompt", prompt);
+      //var group = Groups.findOne({'_id': this.groupIDs[0]});
+      //Session.set("currentGroup", group);
+      //var user = Session.get("currentUser");
+      //if (prompt) {
+        //logger.trace("found current prompt with id: " + prompt._id);
+        //Session.set("currentPrompt", prompt);
+        //logger.debug("Prompt selected");
+        //Router.go('HcompDashboard', 
+            //{promptID: prompt._id, userID: user._id});
+      //} else {
+        //logger.error("couldn't find current prompt with id: " + 
+            //prompt._id);
+      //}
+    //},
+    //'click .review-button': function () {
+      //// Set the current prompt
+      //var prompt = Prompts.findOne({'_id': this._id});
+      //Session.set("currentPrompt", prompt);
+      //var group = Groups.findOne({'_id': this.groupIDs[0]});
+      //Session.set("currentGroup", group);
+      //var user = Session.get("currentUser");
+      //if (prompt) {
+        //logger.trace("found current prompt with id: " + prompt._id);
+        //Session.set("currentPrompt", prompt);
+        //logger.debug("Prompt selected");
+        //Router.go('Visualization', {promptID: prompt._id, userID: user._id});
+      //} else {
+        //logger.error("couldn't find current prompt with id: " + 
+            //prompt._id);
+      //}
+    //},
+});
+
+/********************************************************************
+ * Experiments tab helpers and events
+ * *****************************************************************/
 Template.CrowdExperiment.helpers({
   desc: function() {
     return this.description;
@@ -140,6 +282,26 @@ Template.CrowdExperiment.helpers({
     }
   },
 });
+Template.NewExperimentModal.helpers({
+  prompts: function() {
+    logger.trace("Getting prompts...");
+    return Prompts.find();
+  },
+});
+
+Template.ExperimentPromptItem.helpers({
+  question: function() {
+    return this.question;
+  },
+  timeLimit: function() {
+    if (this.length > 0) {
+      return this.length;
+    } else {
+      return "No time limit";
+    }
+  },
+});
+
 
 Template.CrowdExperimentExcludeUser.helpers({
   thisID: function() {
@@ -295,175 +457,6 @@ Template.CondParticipant.helpers({
   },
 });
 
-Template.CrowdBrainstorm.helpers({
-  question: function() {
-    return this.question;
-  },
-  getData: function() {
-    logger.debug("Data context: " + JSON.stringify(this._id));
-    logger.debug("current user: " + JSON.stringify(Session.get("currentUser")));
-    var result = {'promptID': this._id, 'userID': Session.get("currentUser")._id};
-    logger.debug("Data object: " + JSON.stringify(result));
-    return result;
-  },
-  participants: function() {
-    logger.debug("current prompt: " + JSON.stringify(this));
-    var groups = Groups.find({_id: {$in: this.groupIDs}});
-    var users = [];
-    groups.forEach(function(group) {
-      users = users.concat(group.users);
-    });
-    return users.map(function(user) {
-      return user.name;
-    });
-  },
-  hasTime: function() {
-    if (this.hasOwnProperty("length")) {
-      logger.trace("Prompt has session length defined: " + this.length);
-      if (this.length > 0) {
-        return true;
-      } else {
-        return false;
-      } 
-    } else {
-      logger.trace("Prompt does not have session length defined");
-      return false;
-    }
-  },
-  hasUsers: function() {
-    var groups = Groups.find({_id: {$in: this.groupIDs}});
-    var users = [];
-    groups.forEach(function(group) {
-    logger.debug("Looking at groups: " + JSON.stringify(group));
-      logger.debug("group has users: " + JSON.stringify(group.users));
-      users = users.concat(group.users);
-      logger.debug("users list is now: " + JSON.stringify(users));
-    });
-    logger.debug("list of users:" + JSON.stringify(users));
-    if (users.length > 0) {
-      logger.trace("Prompt has users");
-      return true;
-    } else {
-      logger.trace("Prompt has no users");
-      return false;
-    }
-  },
-  numWorkers: function() {
-    logger.debug("current prompt: " + JSON.stringify(this));
-    var groups = Groups.find({_id: {$in: this.groupIDs}});
-    var users = [];
-    groups.forEach(function(group) {
-      users = users.concat(group.users);
-    });
-    return users.length;
-  },
-  formUrl: function() {
-    //Get absolute base url and trim trailing slash
-    return Meteor.absoluteUrl().slice(0,-1);
-  },
-  promptID: function() {
-    return {promptID: this._id};
-  },
-  isNotPrepped: function() {
-    var prompt = Prompts.findOne({_id: this._id})
-    logger.trace(prompt)
-    if (typeof this['forestGraphID'] === 'undefined') {
-      return true;
-    } else {
-      return false;      
-    }
-  },
-});
-
-Template.CrowdBrainstorm.events({
-  //'click .dash-button': function() {
-    //console.log("go to dash");
-    //Router.go("HcompDashboard", 
-      //{promptID: this._id,
-        //userID: Session.get('currentUser')});
-  //},
-  //'click .review-button': function() {
-    //console.log("go to reviewpage");
-    //Router.go("HcompResultsPage", 
-      //{promptID: this._id, userID: Session.get("currentUser")});
-  //},
-  'click .prep-forest': function () {
-    logger.debug("Prepping db for dataforest analysis");
-    logger.debug(this);
-    var prompt = this;
-    var group = Groups.findOne({_id: prompt.groupIDs[0]});
-    var user = Session.get("currentUser");
-    var type = "data_forest";
-    var data = {is_processed: false};
-    graphID = GraphManager.createGraph(prompt, group, user, type, data);
-    Prompts.update({_id: prompt._id}, {$set: {forestGraphID: graphID}});
-  },
-});
-
-/********************************************************************
- * Template function returning a boolean if there is a logged in user
- * *****************************************************************/
-
-Template.PromptsTab.events({
-    'click button.createPrompt': function () {
-      var newQuestion = $("input#prompt-text").val();
-      var newTitle = $("input#prompt-title").val();
-      var minutes = parseInt($("input#prompt-length").val());
-      logger.debug("Session will be: " + minutes + " long");
-      var newPrompt = PromptManager.create(newQuestion);
-      if (newTitle.trim() !== "") {
-        PromptManager.setTitle(newPrompt, newTitle);
-      }
-      if (!isNaN(minutes)) {
-        PromptManager.setLength(newPrompt, parseInt(minutes));
-      }
-      var group = GroupManager.create(newPrompt.template);
-      PromptManager.addGroups(newPrompt, group);
-      GroupManager.addUser(group, Session.get("currentUser"),
-          RoleManager.defaults['HcompFacilitator'].title);
-      //Clear textfield values
-      $("input#prompt-text").val("");
-      $("input#prompt-title").val("");
-      $("input#prompt-length").val(0);
-      
-    },
-    'click .dash-button': function () {
-      // Set the current prompt
-      var prompt = Prompts.findOne({'_id': this._id});
-      Session.set("currentPrompt", prompt);
-      var group = Groups.findOne({'_id': this.groupIDs[0]});
-      Session.set("currentGroup", group);
-      var user = Session.get("currentUser");
-      if (prompt) {
-        logger.trace("found current prompt with id: " + prompt._id);
-        Session.set("currentPrompt", prompt);
-        logger.debug("Prompt selected");
-        Router.go('HcompDashboard', 
-            {promptID: prompt._id, userID: user._id});
-      } else {
-        logger.error("couldn't find current prompt with id: " + 
-            prompt._id);
-      }
-    },
-    'click .review-button': function () {
-      // Set the current prompt
-      var prompt = Prompts.findOne({'_id': this._id});
-      Session.set("currentPrompt", prompt);
-      var group = Groups.findOne({'_id': this.groupIDs[0]});
-      Session.set("currentGroup", group);
-      var user = Session.get("currentUser");
-      if (prompt) {
-        logger.trace("found current prompt with id: " + prompt._id);
-        Session.set("currentPrompt", prompt);
-        logger.debug("Prompt selected");
-        Router.go('Visualization', {promptID: prompt._id, userID: user._id});
-      } else {
-        logger.error("couldn't find current prompt with id: " + 
-            prompt._id);
-      }
-    },
-
-});
 
 Template.ExperimentsTab.events({
     'click button.createExp': function () {
@@ -493,74 +486,9 @@ Template.ExperimentsTab.events({
 
     },
 });
-Template.CrowdPromptPage.events({
-    'click .review-button': function () {
-      // Set the current prompt
-      var prompt = Prompts.findOne({'_id': this._id});
-      Session.set("currentPrompt", prompt);
-      var group = Groups.findOne({'_id': this.groupIDs[0]});
-      Session.set("currentGroup", group);
-      var user = Session.get("currentUser");
-      if (prompt) {
-        logger.trace("found current prompt with id: " + prompt._id);
-        Session.set("currentPrompt", prompt);
-        logger.debug("Prompt selected");
-        Router.go('Visualization', {promptID: prompt._id, userID: user._id});
-      } else {
-        logger.error("couldn't find current prompt with id: " + 
-            prompt._id);
-      }
-    },
-});
 
 Template.CrowdExperiment.events({
-  // 'click .exp-dash-button': function () {
-    // var exp = Experiments.findOne({_id: this._id});
-    // Session.set("currentExp", exp);
-    // var user = Session.get("currentUser");
-    // if (exp) {
-      // logger.trace("found current exp with id: " + this._id);
-      // var prompt = Prompts.findOne({'_id': exp.promptID});
-      // Session.set("currentPrompt", prompt);
-      // var group = Groups.findOne({'_id': prompt.groupIDs[0]});
-      // Session.set("currentGroup", group);
-      // logger.debug("Prompt selected");
-      // Router.go('ExpDashboard', 
-          // {promptID: prompt._id, 
-            // userID: user._id,
-            // expID: exp._id});
-    // } else {
-      // logger.warn("couldn't find current exp with id: " + 
-          // this._id);
-    // }
-  // },
-
-// not used at the moment
-  'click .begin-bs': function () {
-    // var condName = this.description;
-    // logger.trace("Begin brainstorm for " + condName + " condition");
-    // var exp = Experiments.findOne({_id: this.expID});
-    // var userIDs = ExperimentManager.getUsersInCond(exp, condName);
-    // userIDs.forEach(function (id) {
-    //   logger.debug("Updating route for user with id: " + id);
-    //   var routeName = "MturkIdeation" + condName;
-    //   logger.debug("Sending to route: " + routeName)
-    //   MyUsers.update({_id: id}, {$set: {'route': routeName}});
-    // });
-  },
-
-  // 'click .rm-excl-user': function() {
-  //   var userName = this;
-  //   // var expID = this.
-  //   var expContainer = $(event.target).parents('.exclude-user');
-  //   var expID = expContainer.attr("id").split("-")[1];
-  //   var exp = Experiments.findOne({_id: expID});
-  //   logger.trace("This experiment: " + JSON.stringify(exp));
-  //   // console.log(expID);
-  //   // var msg = JSON.stringify(expID);
-  //   // alert(msg);
-  //   // logger.trace("The experiment: " + )
-  //   alert("Remove " + userName + " from excludeUsers in " + expID +"?");
-  //   ExperimentManager.removeExcludeUser(expID, userName)
-  // }
 });
+/********************************************************************
+ * Data Processing tab helpers and events
+ * *****************************************************************/
