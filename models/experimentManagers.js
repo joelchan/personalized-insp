13b0186dc7f8cd16ee9dbdb20683dfa4ed19f1e6
Joel Chan <chozen86@gmail.com>
2015-09-18 02:24:29 -0400

    sample based on how many have completed, not assigned

--- models/experimentManagers.js
+++ models/experimentManagers.js
@@ -324,7 +324,7 @@ ExperimentManager = (function () {
               //  assigned
               var numPartWanted = exp.conditions[i].partNum; // desired number of participants
               var cond = Conditions.findOne({_id: exp.conditions[i]._id})
-              var numPartAssigned = cond.assignedParts.length; // number of participants currently assigned
+              var numPartAssigned = cond.completedParts.length; // number of participants currently assigned
               logger.debug(numPartWanted + " participants wanted for " + cond.description + " condition, " + numPartAssigned + " assigned so far");
               
               // Square the number to heavily bias in favor conditions with fewer assigned participants
